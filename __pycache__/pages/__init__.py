e1a72ca33f7943b7b7aec8b09c81bfa7f74e522d
boxheadguy231 <jjchan.as1@gmail.com>
2025-09-25 14:48:34 +0800

    new distributed

---
+++ __pycache__/pages/__init__.py
@@ -0,0 +1,7 @@
+# pages/__init__.py
+from .login import LoginPage
+from .register import RegisterPage
+from .dashboard import DashboardPage
+from .analysis import AnalysisPage
+
+__all__ = ["LoginPage", "RegisterPage", "DashboardPage", "AnalysisPage"]
